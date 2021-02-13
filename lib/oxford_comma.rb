96d8fc303f0faaa12f8c1326a84caa17f26269e5
Omar Branez <omar.branez@gmail.com>
2021-02-13 10:00:59 -0500

    commit tree f27f041906525f5b3576aa3f2cd92802d7af79c5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
     if array.length == 1
-        array.join
+        array.join #if there's only one element in the array, it can display as is
     elsif array.length == 2
-        array.join(" and ")
+        array.join(" and ") #if there's two elements in the array, we need to separate the strings with a space and "and" first
     elsif array.length >= 3
-        array[-1].insert(0, "and ") && array.join(", ")
+        array.last.insert(0, "and ") && array.join(", ")
+        #for the final element of the array, add "and " at index 0 (the beginning), while separating the other elements with a comma and a space after each one
     end
 end
